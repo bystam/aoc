2d9b7b53e6c0d9a3b44a952bc75a6bf6efc38aaa
Fredrik Bystam <fredrik.bystam@kry.se>
2018-12-05 22:11:11 +0100

    Initial Commit

---
+++ AOC/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AOC
+//
+//  Created by Fredrik Bystam on 2018-12-05.
+//  Copyright © 2018 Fredrik Bystam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
