a6f993fa8ddd448b65da7d4b2c434ede881a7d9b
Fredrik Tõnisson-Bystam <fredrik.bystam@hedvig.com>
2023-12-18 12:32:18 +0100

    I hate day 12 so much

--- aoc23/src/main/kotlin/Day12.kt
+++ aoc23/src/main/kotlin/Day12.kt
@@ -14,20 +14,15 @@ object Day12: Day {
     """.trimIndent()
 
     override fun task1(input: Input): Any {
-//        println("???.### 1,1,3 = " + Record.from("???.### 1,1,3").countTotalArrangements())
-//        println(".??..??...?##. 1,1,3 = " + Record.from(".??..??...?##. 1,1,3").countTotalArrangements())
-        println("?#?#?#?#?#?#?#? 1,3,1,6 = " + Record.from("?#?#?#?#?#?#?#? 1,3,1,6").countTotalArrangements())
-//        println("????.#...#... 4,1,1 = " + Record.from("????.#...#... 4,1,1").countTotalArrangements())
-//        println("????.######..#####. 1,6,5 = " + Record.from("????.######..#####. 1,6,5").countTotalArrangements())
-//        println("?###???????? 3,2,1 = " + Record.from("?###???????? 3,2,1").countTotalArrangements())
         return input.lines.map { Record.from(it) }.sumOf { it.countTotalArrangements() }
     }
 
     override fun task2(input: Input): Any {
-//        println(
-//            Record.from("????.######..#####. 1,6,5").unfolded().countTotalArrangements()
-//        )
-        return ""// input.lines.map { Record.from(it).unfolded() }.sumOf { it.countTotalArrangements() }
+        val records = input.lines.map { Record.from(it).unfolded() }
+
+        return records.sumOf {
+            it.countTotalArrangements()
+        }
     }
 
     data class Record(
@@ -36,95 +31,28 @@ object Day12: Day {
     ) {
 
         fun unfolded(): Record = Record(
-            arrangement = arrangement + arrangement + arrangement + arrangement + arrangement,
+            arrangement = "$arrangement?$arrangement?$arrangement?$arrangement?$arrangement",
             brokenGroups = brokenGroups + brokenGroups + brokenGroups + brokenGroups + brokenGroups
         )
 
-        fun countTotalArrangements(): Int {
-//            return countTotalArrangements(arrangement)
-            return countTotalArrangements2(
-                brokenGroups, 0
-            )
+        fun countTotalArrangements(): Long {
+            return count(arrangement, brokenGroups, mutableMapOf())
         }
 
-        private fun countTotalArrangements2(
-            brokenGroupsLeft: List<Int>,
-            fromIndex: Int
-        ): Int {
-            val group = brokenGroupsLeft.firstOrNull() ?: return 1
-            var sum = 0
-            var index = fromIndex
-            while (true) {
-                val beyondGroup = (index+group)
-                if (beyondGroup > arrangement.length) {
-                    break
-                }
-                val isPossibleMatch = arrangement.substring(index..<beyondGroup).all {
-                    it == '#' || it == '?'
-                }
-                if (isPossibleMatch) {
-                    sum += countTotalArrangements2(
-                        brokenGroupsLeft.drop(1), beyondGroup + 1
-                    )
-                }
-                index += 1
-            }
-            return sum
-        }
+        // stolen from https://github.com/eagely/adventofcode/blob/main/src/main/kotlin/solutions/y2023/Day12.kt
+        // because I suck at programming and I hate this day
+        private fun count(config: String, groups: List<Int>, cache: MutableMap<Pair<String, List<Int>>, Long>): Long {
+            if (groups.isEmpty()) return if ("#" in config) 0 else 1
+            if (config.isEmpty()) return 0
 
-        private fun countTotalArrangements(
-            arrangementSoFar: String
-        ): Int {
-            val brokenGroupsSoFar = brokenGroups(arrangementSoFar)
-            val doneGroupsSoFar = brokenGroupsSoFar.dropLast(1)
-
-            if (brokenGroupsSoFar.size > brokenGroups.size) {
-                return 0 // impossible
+            return cache.getOrPut(config to groups) {
+                var result = 0L
+                if (config.first() in ".?")
+                    result += count(config.drop(1), groups, cache)
+                if (config.first() in "#?" && groups.first() <= config.length && "." !in config.take(groups.first()) && (groups.first() == config.length || config[groups.first()] != '#'))
+                    result += count(config.drop(groups.first() + 1), groups.drop(1), cache)
+                result
             }
-            for ((doneGroup, realGroup) in doneGroupsSoFar.zip(brokenGroups)) {
-                if (doneGroup != realGroup) {
-                    return 0 // impossible
-                }
-            }
-            if (brokenGroupsSoFar.isNotEmpty()) {
-                if (brokenGroupsSoFar[brokenGroupsSoFar.lastIndex] > brokenGroups[brokenGroupsSoFar.lastIndex]) {
-                    return 0
-                }
-            }
-
-            val nextUnknown = arrangementSoFar.indexOf('?')
-            if (nextUnknown == -1) return if (brokenGroupsSoFar == brokenGroups) 1 else 0
-
-            val ifOperational = countTotalArrangements(
-                arrangementSoFar = arrangementSoFar.replaceRange(nextUnknown, nextUnknown + 1, ".")
-            )
-            val ifBroken = countTotalArrangements(
-                arrangementSoFar = arrangementSoFar.replaceRange(nextUnknown, nextUnknown + 1, "#")
-            )
-            return ifOperational + ifBroken
-        }
-
-        private fun brokenGroups(arrangementSoFar: String): List<Int> {
-            val groups = mutableListOf<Int>()
-
-            var currentGroup: Int = 0
-            for (index in arrangementSoFar.indices) {
-                when (arrangementSoFar[index]) {
-                    '#' -> currentGroup += 1
-                    '.' -> {
-                        if (currentGroup > 0) {
-                            groups += currentGroup
-                            currentGroup = 0
-                        }
-                    }
-                    '?' -> break
-                }
-            }
-            if (currentGroup > 0) {
-                groups += currentGroup
-                currentGroup = 0
-            }
-            return groups
         }
 
         companion object {
